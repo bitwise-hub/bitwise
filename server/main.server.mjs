00940cf3f2ad61891eb5c4314a802f6562e0b5c5
Sai Krishna Gupta <guptasai@sheridancollege.ca>
2024-12-27 13:03:52 -0500

    article formatting and other stuff

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5VVP4BJW.mjs";import"./chunk-TFOZYXF5.mjs";import"./chunk-REUUYMGY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HN3QBRIW.mjs";import"./chunk-SMRMSOUB.mjs";import"./chunk-LUU6NPP4.mjs";import"./chunk-R3TAX4SW.mjs";export{a as default};
